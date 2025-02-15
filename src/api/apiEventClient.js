80edea0827827b7dbfde5333b4c72ced12f53665
Dilemma086 <Dilemma086@yandex.ru>
2025-02-15 11:53:42 +0300

    Обновление API

--- src/api/apiEventClient.js
+++ src/api/apiEventClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000'
-})
+    baseURL: process.env.REACT_APP_SERVER_URL
+});
 
 export const eventAPI ={
     getEvents(){ 
