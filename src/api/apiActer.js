514e0e657050b82b8d1166198c539c3227b511d7
Dilemma086 <Dilemma086@yandex.ru>
2025-02-15 15:03:24 +0300

    Возвращение API

--- src/api/apiActer.js
+++ src/api/apiActer.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URL
-});
+    baseURL: 'http://localhost:5000'
+})
 
 
 export const acterDashBoarAPI ={
